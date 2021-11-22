863badaec50536b724923e5709d7ff5f4d343240
SlayZar <vaskind@yandex.ru>
2021-11-22 23:10:43 +0300

    Shap added

--- scor.py
+++ scor.py
@@ -10,6 +10,16 @@ import plotly.figure_factory as ff
 import scipy.cluster.hierarchy as sch
 from sklearn.manifold import TSNE
 import streamlit.components.v1 as components
+from matplotlib import pyplot as pl
+
+import xgboost as xgb
+from xgboost import XGBClassifier
+import shap
+import lightgbm as lgb
+import joblib
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 
 def get_table_download_link(df):
     csv = df.to_csv(index=False)
@@ -80,6 +90,64 @@ def visualisation(df, numeric_columns, non_numeric_columns):
 # data_path = 'data/test.csv'
 # target_col_name = 'pred2_bin'
 
+def preproc(customers):
+    df = pd.pivot_table(customers, index = ['CustomerID'], columns = ['Gender'], 
+                    values = ['Age', 'Annual Income (k$)', 'Spending Score (1-100)'],
+                    aggfunc = 'mean').fillna(0)
+    df.columns = ['Female_age', 'Male_age', 'Female_income', 'Male_Income', 'Female_score', 'Male_score']
+    return df
+
+
+def interpret(df):
+    from config import MODEL_COLS, MODEL_PATH, TARGET_COL
+    df = preproc(df)
+#     st.write(df[MODEL_COLS])
+    clf = joblib.load('models/clf') #Закомментить
+    df[TARGET_COL] = clf.predict(df[MODEL_COLS]) #Закомментить
+    loaded_model_cb = CatBoostClassifier()
+    loaded_model_cb.load_model('models/cb') 
+#     loaded_model = XGBClassifier()
+#     loaded_model.load_model(MODEL_PATH) 
+    gbm = joblib.load('models/lightgbm')
+    lgb_train = lgb.Dataset(df[MODEL_COLS], df[TARGET_COL])
+
+    
+    ntree=st.number_input('Select the desired record for detailed explanation on the training set'
+                                       , min_value=0
+                                       , max_value=df.shape[0]
+                                       )
+    graph = lgb.create_tree_digraph(gbm, tree_index=ntree, name='Tree54')
+    st.graphviz_chart(graph)
+#     tree=xgb.to_graphviz(loaded_model,num_trees=ntree)
+#     st.graphviz_chart(tree)
+#     importance_type = st.selectbox('Select the desired importance type', ('weight','gain','cover'),index=0)
+#     importance_plot = xgb.plot_importance(loaded_model,importance_type=importance_type)
+#     pl.title ('xgboost.plot_importance(best XGBoost model) importance type = '+ str(importance_type))
+#     st.pyplot(bbox_inches='tight')
+#     pl.clf()
+#     st.write('To handle this inconsitency, SHAP values give robust details, among which is feature importance')
+    explainer = shap.TreeExplainer(loaded_model_cb)
+    shap_values = explainer.shap_values(df[MODEL_COLS])
+    pl.title('Assessing feature importance based on Shap values')
+    shap.summary_plot(shap_values,df[MODEL_COLS],plot_type="bar",show=False)
+    st.pyplot(bbox_inches='tight')
+    pl.clf()
+    expectation = explainer.expected_value
+    individual = st.number_input('Select the desired record from the training set for detailed explanation.'
+                                            , min_value=0
+                                       , max_value=df.shape[0]
+                                       )
+    predicted_values = loaded_model_cb.predict(df[MODEL_COLS])
+    real_value = df[TARGET_COL]    
+    shap.force_plot(explainer.expected_value[0], shap_values[0][individual], # Убрать нолики
+                    df[MODEL_COLS].iloc[individual,:],matplotlib=True,show=False
+                    ,figsize=(16,5))
+    st.pyplot(bbox_inches='tight',dpi=300,pad_inches=0)
+    pl.clf()
+    if st.button('Click here to see a drilldown of the SHAP values'):
+        shap_table=pd.DataFrame(shap_values,columns=MODEL_COLS)
+        st.table(shap_table.iloc[individual])
+
 def eda_func(df):
     if st.checkbox("Показать 10 строк датафрейма"):
         st.write(df.head(10))           
@@ -90,17 +158,16 @@ def eda_func(df):
     non_numeric_columns = list(set(df.select_dtypes(['object']).columns).difference(dropcols))
     non_numeric_columns.append(None)
     options2 = st.selectbox('Что хотим сделать?',
-             ['Посмотреть зависимости (EDA)', 'Кластеризация'], index=0)
+             ['Посмотреть зависимости (EDA)', 'Кластеризация', 'Интерпретация модели'], index=0)
     if options2 == 'Посмотреть зависимости (EDA)':
         visualisation(df, numeric_columns, non_numeric_columns)
     elif options2 == 'Кластеризация': 
         hierarch(df)
+    elif options2 == 'Интерпретация модели': 
+        interpret(df)
         
 def hierarch(customers):
-    df = pd.pivot_table(customers, index = ['CustomerID'], columns = ['Gender'], 
-                    values = ['Age', 'Annual Income (k$)', 'Spending Score (1-100)'],
-                    aggfunc = 'mean').fillna(0)
-    df.columns = ['Female_age', 'Male_age', 'Female_income', 'Male_Income', 'Female_score', 'Male_score']
+    df = preproc(df)
     X_embedded = TSNE(n_components=2, perplexity = 5, verbose = 0).fit_transform(df.to_numpy())
     vis_df = pd.DataFrame(X_embedded, index = df.index)
     fig = ff.create_dendrogram(np.array(vis_df[[0,1]]),
